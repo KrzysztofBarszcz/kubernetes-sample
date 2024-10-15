ae5cb6fea62053aecf973d2f02da07e71dc5be9f
Krzysztof Barszcz <krzysztof.barszcz.92@gmail.com>
2024-10-15 19:50:54 +0200

    (#1) Create basic jib app

---
+++ server/src/main/java/com/github/krzysztofbarszcz/HelloController.java
@@ -0,0 +1,16 @@
+package com.github.krzysztofbarszcz;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello " + name;
+    }
+}
